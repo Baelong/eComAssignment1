27b4cff96628ba09da835b95cafd188ac6b9a103
%username% <%fruhamsan@gmail.com%>
2024-02-23 22:42:32 -0500

    part of ali working

---
+++ app/controllers/Main.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\controllers;
+
+use stdClass;
+
+class Main extends \app\core\Controller{
+
+	function index(){
+	$this->view('Main/index');
+	}
+	
+	function about_us(){
+	$this->view('Main/about_us');
+	}
+}

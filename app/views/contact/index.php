6289b6d3d484e0192f7d50348174585472eb4507
%username% <%fruhamsan@gmail.com%>
2024-02-23 23:00:03 -0500

    ali part functional just missing images

--- app/views/contact/index.php
+++ app/views/contact/index.php
@@ -10,10 +10,10 @@
 <body>
         <h1>Contact Us</h1>
             <ul>
-                <li><a href="Main/index">Landing page</a></li>
-                <li><a href="Main/about_us">About us</a></li>
-                <li><a href="Contact/index">Contact us</a></li>
-                <li><a href="Contact/read">See the messages we get</a></li>
+                <li><a href="/Main/index">Landing page</a></li>
+                <li><a href="/Main/about_us">About us</a></li>
+                <li><a href="/Contact/index">Contact us</a></li>
+                <li><a href="/Contact/read">See the messages we get</a></li>
             </ul>
         
         <h2>Contact Us</h2>
--- app/views/contact/read.php
+++ app/views/contact/read.php
@@ -5,6 +5,13 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
+	 <h1>Contact Us</h1>
+            <ul>
+                <li><a href="/Main/index">Landing page</a></li>
+                <li><a href="/Main/about_us">About us</a></li>
+                <li><a href="/Contact/index">Contact us</a></li>
+                <li><a href="/Contact/read">See the messages we get</a></li>
+            </ul>
 	<div id='container'>
 		<h1>Contact Us - Messages we get</h1>
 		<table>
